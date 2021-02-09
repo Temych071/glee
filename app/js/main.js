8295a657fa9ace7465d7b6b378a34203849970bd
Temych071 <goto.07@mail.ru>
2021-02-09 21:31:57 +0300

    finished 13th homework

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,25 @@ $(function(){
     dots: true,
   });
 
-  var mixer = mixitup('.products__content');
+  $('.slider-partners').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
+  var products_mixer = mixitup('.products__content', {
+    selectors: {
+      control: '.filter-1',
+    }
+  });
+
+  var design_mixer = mixitup('.design__content', {
+    selectors: {
+      control: '.filter-2',
+    }
+  });
+  
 });
